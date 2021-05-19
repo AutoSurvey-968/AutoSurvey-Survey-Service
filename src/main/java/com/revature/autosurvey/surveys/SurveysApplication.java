be4b058abbaee77cf53eab0b4932caba8605f35d
lwood.benjamin <benjamin.wood@revature.net>
2021-05-19 13:13:23 -0500

    Restrict discovery logging & errors

--- src/main/java/com/revature/autosurvey/surveys/SurveysApplication.java
+++ src/main/java/com/revature/autosurvey/surveys/SurveysApplication.java
@@ -1,11 +1,13 @@
 package com.revature.autosurvey.surveys;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 public class SurveysApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/revature/autosurvey/surveys/aspects/LoggingAspect.java
+++ src/main/java/com/revature/autosurvey/surveys/aspects/LoggingAspect.java
@@ -1,5 +1,47 @@
 package com.revature.autosurvey.surveys.aspects;
 
-public class LoggingAspect {
+import java.util.Arrays;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class LoggingAspect {
+	
+	@Around("everything()")
+	public Object log(ProceedingJoinPoint pjp) throws Throwable {
+		Object result = null;
+		Logger log = LoggerFactory.getLogger(pjp.getTarget().getClass());
+		log.debug("Method with signature: {}", pjp.getSignature());
+		log.debug("with arguments: {}", Arrays.toString(pjp.getArgs()));
+		try {
+			result = pjp.proceed();
+		}  catch(Throwable t) {
+			log.error("Method threw exception: {}", t);
+			for(StackTraceElement s : t.getStackTrace()) {
+				log.warn(s.toString());
+			}
+			if(t.getCause() != null) {
+				Throwable t2 = t.getCause();
+				log.error("Method threw wrapped exception: {}", t2);
+				for(StackTraceElement s : t2.getStackTrace()) {
+					log.warn(s.toString());
+				}
+			}
+			throw t; 
+		}
+		log.debug("Method returning with: {}", result);
+		return result;
+	}
+	
+	// hook - a method that only exists as the target for an annotation
+	@Pointcut("execution( * com.revature.autosurvey.surveys..*(..) )")
+	private void everything() { /* Empty method for hook */ }
 }
+
