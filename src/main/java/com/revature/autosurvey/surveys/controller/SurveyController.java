0f2c5c64963b6ae3428872e1bf5d8d1c33cad375
rnbiv45 <robert.bierly4@gmail.com>
2021-05-19 13:59:57 -0400

    add annotations

--- src/main/java/com/revature/autosurvey/surveys/controller/SurveyController.java
+++ src/main/java/com/revature/autosurvey/surveys/controller/SurveyController.java
@@ -1,5 +1,8 @@
 package com.revature.autosurvey.surveys.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class SurveyController {
 	
 	
--- src/main/java/com/revature/autosurvey/surveys/service/SurveyServiceImp.java
+++ src/main/java/com/revature/autosurvey/surveys/service/SurveyServiceImp.java
@@ -1,5 +1,8 @@
 package com.revature.autosurvey.surveys.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class SurveyServiceImp implements SurveyService {
 
 }
