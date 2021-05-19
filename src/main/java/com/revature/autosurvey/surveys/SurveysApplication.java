5671a1e349abae2e6f1186454a213aafcdbb5661
lwood.benjamin <benjamin.wood@revature.net>
2021-05-19 16:19:20 -0500

    Added test

--- src/main/java/com/revature/autosurvey/surveys/SurveysApplication.java
+++ src/main/java/com/revature/autosurvey/surveys/SurveysApplication.java
@@ -1,7 +1,5 @@
 package com.revature.autosurvey.surveys;
 
-import java.util.Arrays;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
