b731c913073d47fd88a1bc15c9ee739c85cfa454
MoldedPixels <MoldedPixels@gmail.com>
2021-05-21 14:07:47 -0400

    fixed the phantom file problem, tests working

--- src/test/java/com/revature/autosurvey/surveys/karate/SurveyKarateTests.java
+++ src/test/java/com/revature/autosurvey/surveys/karate/SurveyKarateTests.java
@@ -8,11 +8,6 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
-import java.io.File;
-import java.util.Collection;
-import net.masterthought.cucumber.Configuration;
-import net.masterthought.cucumber.ReportBuilder;
-import org.apache.commons.io.FileUtils;
 
 class SurveyKarateTests {
 
@@ -21,21 +16,9 @@ class SurveyKarateTests {
 	@Test
 	void testParallel() {
 		System.setProperty("karate.env", "dev");
-		tests.add("/com/revature/autosurvey/surveys/karate/survey-get.feature");
-		tests.add("/com/revature/autosurvey/surveys/karate/survey-post.feature");
+		tests.add("classpath:/com/revature/autosurvey/surveys/karate/survey-get.feature");
+		tests.add("classpath:/com/revature/autosurvey/surveys/karate/survey-post.feature");
 		Results results = Runner.path(tests).parallel(5);
-		generateReport(results.getReportDir());
 		assertEquals(0, results.getFailCount(), results.getErrorMessages());
 	}
-
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	public static void generateReport(String karateOutputPath) {
-		Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] { "json" }, true);
-		List<String> jsonPaths = new ArrayList(jsonFiles.size());
-		jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
-		Configuration config = new Configuration(new File("target"), "AutoSurvey Survey Service");
-		ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
-		reportBuilder.generateReports();
-	}
-
 }
