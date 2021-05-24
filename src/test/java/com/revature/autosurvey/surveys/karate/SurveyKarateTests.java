5a212a67fcd71b1ca224f451898483e65e8b591f
MoldedPixels <MoldedPixels@gmail.com>
2021-05-24 09:53:20 -0400

    created working Karate tests, remaining tests will be developed in their feature branches

--- src/test/java/com/revature/autosurvey/surveys/karate/SurveyKarateTests.java
+++ src/test/java/com/revature/autosurvey/surveys/karate/SurveyKarateTests.java
@@ -8,12 +8,6 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
-<<<<<<< HEAD
-//import net.masterthought.cucumber.Configuration;
-//import net.masterthought.cucumber.ReportBuilder;
-//import org.apache.commons.io.FileUtils;
-=======
->>>>>>> 46d2f6e1e112f00750aec06c958737af65727fd8
 
 class SurveyKarateTests {
 
