c635999857546aa426345a50a58b8a0c58ca33a2
DuskDaleSpider <dakota.ray.clark@gmail.com>
2021-05-27 11:02:06 -0400

    Updated start.sh to use survey-service.jar

--- docker/start.sh
+++ docker/start.sh
@@ -2,4 +2,4 @@
 #Decode the passed in truststore env variable
 mkdir -p src/main/resources
 echo "$TRUSTSTORE_ENCODED" | base64 -d > src/main/resources/cassandra_truststore.jks
-java -jar submission-service.jar
+java -jar survey-service.jar
